6132eae66a5af0fd368033eb5f96466cbb6e3cd4
thsa <2492784010@qq.com>
2026-03-12 19:39:00 +0800

    2026.3.12

---
+++ test/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a test from Java executor.");
+        
+        // 测试计算
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
+        }
+        System.out.println("Sum of 1 to 10: " + sum);
+    }
+}
